c808f5c7e60ebde3bedc360b825bbdcdc28e9d34
Abegail Clemente <lr.aclemente@mmdc.mcl.edu.ph>
2025-01-25 00:34:28 +0800

    Commit

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,13 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
 
 class User(models.Model):
-    username = models.CharField(max_length=100, unique=True)
-    email = models.EmailField(unique=True)
+    username = models.CharField(
+        max_length=50,
+        validators=[RegexValidator(regex='^[a-zA-Z0-9]*$', message='Username must be alphanumeric', code="invalid_username")] #add validation here, username should be alphanumeric
+    )
+    email = models.EmailField(unique=True) # EmailField is a built-in email validator
     created_at = models.DateTimeField(auto_now_add=True)
 
 
@@ -12,9 +16,9 @@ class User(models.Model):
 
 
 class Post(models.Model):
-    content = models.TextField()
-    author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField() # The main content of the post
+    author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE) # Links the post to its author
+    created_at = models.DateTimeField(auto_now_add=True) # Timestamp for when the post is created
 
 
     def __str__(self):
@@ -22,10 +26,11 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    text = models.TextField()
-    author = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(blank=False) # The main content of the comment
+                                         # add a field level validation, blank field will not be accepted
+    author = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE) # Links the comment to its author
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE) # Links the comment to a specific post
+    created_at = models.DateTimeField(auto_now_add=True) # Timestamp for when the comment is created
 
 
     def __str__(self):
--- posts/urls.py
+++ posts/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from . import views
+from .views import UserListCreate, PostListCreate, CommentListCreate
 
 urlpatterns = [
-            path('users/', views.get_users, name='get_users'), # Get the users
-            path('users/create/', views.create_user, name='create_user'), # Create a user
-            path('users/update/<int:id>/', views.update_user, name='update_user'), # Update a user
-            path('users/delete/<int:id>/', views.delete_user, name='delete_user'), # Delete a user
-            path('posts/create/', views.create_post, name='create_posts'), # Create post
-            path('posts/', views.get_posts, name='get_posts'), # Get posts
+            path('users/', UserListCreate.as_view(), name='user-list-create'), # updated get users and post users
+            path('posts/', PostListCreate.as_view(), name='post-list-create'), # updated get and post 'posts'
+            path('comments/', CommentListCreate.as_view(), name='comment-list-create'), # get & post comments
+            #path('users/', views.get_users, name='get_users'), # Get the users
+            #path('users/create/', views.create_user, name='create_user'), # Create a user
+            #path('users/update/<int:id>/', views.update_user, name='update_user'), # Update a user
+            #path('users/delete/<int:id>/', views.delete_user, name='delete_user'), # Delete a user
+            #path('posts/create/', views.create_post, name='create_posts'), # Create post
+            #path('posts/', views.get_posts, name='get_posts'), # Get posts
 
         ]
