50a6e8df72cb05d82a67d54e934871ddd3325a27
Abegail Clemente <lr.aclemente@mmdc.mcl.edu.ph>
2025-01-25 12:16:41 +0800

    added new validations

--- posts/serializers.py
+++ posts/serializers.py
@@ -5,21 +5,34 @@ from .models import User, Post, Comment
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'created_at']
+        fields = ['id', 'username', 'email', 'password', 'created_at']
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        error_messages={'does_not_exist': 'Author not found.'}
+    )
+    
     comments = serializers.StringRelatedField(many=True, read_only=True)
 
 
     class Meta:
         model = Post
         fields = ['id', 'content', 'author', 'created_at', 'comments']
-    
-    
+       
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    post = serializers.PrimaryKeyRelatedField(
+        queryset=Post.objects.all(),
+        error_messages={'does_not_exist': 'Post not found.'}
+    )
+    author = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        error_messages={'does_not_exist': 'Author not found.'}
+    )
+
     class Meta:
         model = Comment
         fields = ['id', 'text', 'author', 'post', 'created_at']
